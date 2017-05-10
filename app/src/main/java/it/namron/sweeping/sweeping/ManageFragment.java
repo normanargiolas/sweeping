3bf714784304a5c78b3ae60cd6159aa58c46a9b6
NormaN <normanargiolas@gmail.com>
2017-05-10 02:05:16 +0200

    fab

--- app/src/main/java/it/namron/sweeping/sweeping/ManageFragment.java
+++ app/src/main/java/it/namron/sweeping/sweeping/ManageFragment.java
@@ -37,7 +37,9 @@ public class ManageFragment extends Fragment {
     /*
      * This number will uniquely identify our FilePickerActivity Intent for get directory path
      */
-    private static final int PICKFILE_CODE_INTENT = 1;
+    private static final int PICKFILE_SOURCE_CODE_INTENT = 1;
+    private static final int PICKFILE_DESTINATION_CODE_INTENT = 2;
+
 
     /*
      * This number will uniquely identify our Loader for get directory size
@@ -179,6 +181,17 @@ public class ManageFragment extends Fragment {
             };
         }
 
+        private int countFiles(File directory) {
+            int count = 0;
+            for (File file : directory.listFiles()) {
+                if (file.isDirectory()) {
+                    count += countFiles(file);
+                }
+                count++;
+            }
+            return count;
+        }
+
         @Override
         public void onLoadFinished(Loader<Integer> loader, Integer data) {
             Log.d(LOG_TAG, "onLoadFinished");
@@ -204,23 +217,19 @@ public class ManageFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_manage, container, false);
 
-        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab_source_manage = (FloatingActionButton) rootView.findViewById(R.id.fab_source_manage);
+        fab_source_manage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
                 try {
-
-//                    Utils.makeHepticFeedback(getActivity());
-
                     Intent selectDirectoyIntent = new Intent(getActivity(), FilePickerActivity.class);
                     selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
                     selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true);
                     selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
                     selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
-                    startActivityForResult(selectDirectoyIntent, PICKFILE_CODE_INTENT);
+                    startActivityForResult(selectDirectoyIntent, PICKFILE_SOURCE_CODE_INTENT);
 
                 } catch (Exception e) {
                     Log.e(LOG_TAG, "exception", e);
@@ -228,17 +237,33 @@ public class ManageFragment extends Fragment {
 
                     Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT).show();
                 }
-
-
-//                Uri selectedUri = Uri.parse(Environment.getExternalStorageDirectory() + "/");
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.setDataAndType(selectedUri, "resource/folder");
-//                startActivityForResult(intent, PICKFILE_CODE_INTENT);
-
-
             }
         });
 
+
+        FloatingActionButton fab_destination_manage = (FloatingActionButton) rootView.findViewById(R.id.fab_destination_manage);
+        fab_destination_manage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                try {
+                    Intent selectDirectoyIntent = new Intent(getActivity(), FilePickerActivity.class);
+                    selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+                    selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true);
+                    selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
+                    selectDirectoyIntent.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
+                    startActivityForResult(selectDirectoyIntent, PICKFILE_DESTINATION_CODE_INTENT);
+
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "exception", e);
+                    e.printStackTrace();
+
+                    Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         /*
          * Initialize the loader
          */
@@ -250,50 +275,87 @@ public class ManageFragment extends Fragment {
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && requestCode == PICKFILE_CODE_INTENT) {
-            if (null != data && !data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
-                Log.d(LOG_TAG, "EXTRA_ALLOW_MULTIPLE");
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case PICKFILE_SOURCE_CODE_INTENT:
+                    if (null != data && !data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
+                        Log.d(LOG_TAG, "EXTRA_ALLOW_MULTIPLE");
 
-                // The URI will now be something like content://PACKAGE-NAME/root/path/to/file
-                Uri uri = data.getData();
-                // A utility method is provided to transform the URI to a File object
-                File file = com.nononsenseapps.filepicker.Utils.getFileForUri(uri);
-                // If you want a URI which matches the old return value, you can do
-                Uri fileUri = Uri.fromFile(file);
-                // Do something with the result...
-
-                manageResoult(fileUri);
-
-                String msg = "Selected dir" + fileUri.getPath();
-
-                Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
-                Log.d(LOG_TAG, msg);
-
-
-            } else {
-                Log.d(LOG_TAG, "NOT EXTRA_ALLOW_MULTIPLE");
-
-
-                // Handling multiple results is one extra step
-                ArrayList<String> paths = data.getStringArrayListExtra(FilePickerActivity.EXTRA_PATHS);
-                if (paths != null) {
-                    for (String path : paths) {
-                        Uri uri = Uri.parse(path);
-                        // Do something with the URI
+                        // The URI will now be something like content://PACKAGE-NAME/root/path/to/file
+                        Uri uri = data.getData();
+                        // A utility method is provided to transform the URI to a File object
                         File file = com.nononsenseapps.filepicker.Utils.getFileForUri(uri);
                         // If you want a URI which matches the old return value, you can do
                         Uri fileUri = Uri.fromFile(file);
                         // Do something with the result...
 
+                        manageResoult(fileUri);
+
                         String msg = "Selected dir" + fileUri.getPath();
 
                         Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
                         Log.d(LOG_TAG, msg);
+                    } else {
+                        Log.d(LOG_TAG, "NOT EXTRA_ALLOW_MULTIPLE");
+                        // Handling multiple results is one extra step
+                        ArrayList<String> paths = data.getStringArrayListExtra(FilePickerActivity.EXTRA_PATHS);
+                        if (paths != null) {
+                            for (String path : paths) {
+                                Uri uri = Uri.parse(path);
+                                // Do something with the URI
+                                File file = com.nononsenseapps.filepicker.Utils.getFileForUri(uri);
+                                // If you want a URI which matches the old return value, you can do
+                                Uri fileUri = Uri.fromFile(file);
+                                // Do something with the result...
+                                String msg = "Selected dir" + fileUri.getPath();
+                                Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+                                Log.d(LOG_TAG, msg);
 
+                            }
+                        }
                     }
-                }
-            }
+                    break;
+                case PICKFILE_DESTINATION_CODE_INTENT:
+                    if (null != data && !data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
+                        Log.d(LOG_TAG, "EXTRA_ALLOW_MULTIPLE");
 
+                        // The URI will now be something like content://PACKAGE-NAME/root/path/to/file
+                        Uri uri = data.getData();
+                        // A utility method is provided to transform the URI to a File object
+                        File file = com.nononsenseapps.filepicker.Utils.getFileForUri(uri);
+                        // If you want a URI which matches the old return value, you can do
+                        Uri fileUri = Uri.fromFile(file);
+                        // Do something with the result...
+
+                        manageResoult(fileUri);
+
+                        String msg = "Selected dir" + fileUri.getPath();
+
+                        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(LOG_TAG, msg);
+                    } else {
+                        Log.d(LOG_TAG, "NOT EXTRA_ALLOW_MULTIPLE");
+                        // Handling multiple results is one extra step
+                        ArrayList<String> paths = data.getStringArrayListExtra(FilePickerActivity.EXTRA_PATHS);
+                        if (paths != null) {
+                            for (String path : paths) {
+                                Uri uri = Uri.parse(path);
+                                // Do something with the URI
+                                File file = com.nononsenseapps.filepicker.Utils.getFileForUri(uri);
+                                // If you want a URI which matches the old return value, you can do
+                                Uri fileUri = Uri.fromFile(file);
+                                // Do something with the result...
+                                String msg = "Selected dir" + fileUri.getPath();
+                                Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+                                Log.d(LOG_TAG, msg);
+
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    Log.d(LOG_TAG, "Intent sconosciuto");
+            }
         }
     }
 
@@ -321,14 +383,5 @@ public class ManageFragment extends Fragment {
         }
     }
 
-    public static int countFiles(File directory) {
-        int count = 0;
-        for (File file : directory.listFiles()) {
-            if (file.isDirectory()) {
-                count += countFiles(file);
-            }
-            count++;
-        }
-        return count;
-    }
+
 }
